69089279e0558e342912d21505434105342156b7
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-29 23:03:31 +0300

    The Post model has been described.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111029180627) do
+ActiveRecord::Schema.define(:version => 20111029192904) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(:version => 20111029180627) do
   create_table "forums", :force => true do |t|
     t.string   "title"
     t.integer  "category_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", :force => true do |t|
+    t.integer  "forum_id",   :null => false
+    t.integer  "user_id",    :null => false
+    t.text     "content",    :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
