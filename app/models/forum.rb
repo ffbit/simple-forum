795a41014214d8bcc88fed4c9b8a60b1786f2bbe
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-29 21:18:55 +0300

    bundle exec annotate --position before

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: forums
+#
+#  id          :integer         not null, primary key
+#  title       :string(255)
+#  category_id :integer         not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Forum < ActiveRecord::Base
 end
--- spec/models/forum_spec.rb
+++ spec/models/forum_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: forums
+#
+#  id          :integer         not null, primary key
+#  title       :string(255)
+#  category_id :integer         not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'spec_helper'
 
 describe Forum do
