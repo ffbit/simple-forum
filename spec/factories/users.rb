61b5baf54c8adf0ed719fbec1625ce2a25710aef
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-29 15:55:09 +0300

    Shoulda-matchers and rspec-on-rails-matchers gems have been added.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,12 @@
 
 FactoryGirl.define do
   factory :user do
+    email                 { Factory.next :email }
+    password              "secret"
+    password_confirmation "secret"
+  end
+  
+  sequence :email do |n|
+    "user-#{n}@example.com"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { Factory(:user) }
+  
+  describe "email attribute" do
+    it { should validate_presence_of :email }
+    it { should allow_value("good@email.com").for(:email) }
+    it { should_not allow_value("bad-email@}").for(:email) }
+    it { should validate_uniqueness_of :email }
+  end
+  
+  describe "password" do
+    it { should validate_presence_of :password }
+    it { should validate_confirmation_of :password }
+  end
 end
