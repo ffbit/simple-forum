69b0fb1043d6b5d65e16735fd997029e21a97eb2
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-29 22:34:16 +0300

    Before a migration.

--- db/migrate/20111029192904_create_posts.rb
+++ db/migrate/20111029192904_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-      t.integer :forum_id
-      t.integer :user_id
-      t.text :content
+      t.integer :forum_id, :null => false
+      t.integer :user_id,  :null => false
+      t.text :content,     :null => false
 
       t.timestamps
     end
