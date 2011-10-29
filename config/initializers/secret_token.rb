b90533f8c74e5886c2737c1c2b631efc7ffd5bea
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-29 13:48:22 +0300

    The initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleForum::Application.config.secret_token = 'bb991a1f4d9e72ed5073da878caa6b80bf2dc4b4c0c00e1aacb2114eb8a35fc21e5b61f192638579caf196b2106c175605f90a9c20b77c33bd1effd25cb4c40d'
