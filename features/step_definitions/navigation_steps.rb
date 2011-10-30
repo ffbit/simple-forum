b23563e8ccef2c45a5e4b2ceda7809add740b2f6
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-10-30 19:42:35 +0200

    A Sign out feature is green.

--- features/step_definitions/navigation_steps.rb
+++ features/step_definitions/navigation_steps.rb
@@ -1,3 +1,7 @@
 Given /^I'm on (.*)$/ do |page|
   visit path_to(page)
 end
+
+When /^I visit the sign out page$/ do
+  Given %{I'm on the sign out page}
+end
---
+++ features/step_definitions/users/signout_steps.rb
@@ -0,0 +1,5 @@
+Given /^I've signed in$/ do
+  Given %{I've got a user}
+  Given %{I'm on the sign in page}
+  When %{I sign in}
+end
