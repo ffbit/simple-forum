25644dfc604ea02e05bebeecab77a5d3b2989207
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2011-11-02 10:45:00 +0200

    bundle exec annotate --position before

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111101095736) do
+ActiveRecord::Schema.define(:version => 20111102083531) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(:version => 20111101095736) do
     t.boolean  "admin"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,6 +12,10 @@
 #  admin                  :boolean
 #  created_at             :datetime
 #  updated_at             :datetime
+#  avatar_file_name       :string(255)
+#  avatar_content_type    :string(255)
+#  avatar_file_size       :integer
+#  avatar_updated_at      :datetime
 #
 
 require 'spec_helper'
